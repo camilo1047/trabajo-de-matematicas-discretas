97073b59da7e4f08e9e223d067c49d76d995e170
xHermes16 <xhermes16@gmail.com>
2026-03-08 11:52:48 -0500

    Delete nuevo-proyecto-de-juego directory

--- nuevo-proyecto-de-juego/Scripts/global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-const Tamaño_tile = 32
-var ocupado = {} 
-
-#esta funcion redondea la posicion exacta del mouse para que sea dibisible entre el tamaño de tile
-func organizar_tile(pos: Vector2):
-	return (pos / Tamaño_tile).round() * Tamaño_tile
