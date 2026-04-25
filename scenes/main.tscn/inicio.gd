650d3d15a94e231e648116ff36a671f8d2855238
xHermes16 <xhermes16@gmail.com>
2026-04-25 17:25:35 -0500

    Delete scenes directory

--- scenes/main.tscn/inicio.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	print("=== Juego iniciado ===")
-	print("Dinero: " + str(Jugador.dinero))
-	print("Energía: " + SysEnergia.obtener_estado())
-	print("Tráfico: " + SysTrafico.obtener_estado())
-	print("Reparación: " + SysReparacion.obtener_estado())
